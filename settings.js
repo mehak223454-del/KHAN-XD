1d33036ce83f67895ad32166ca365516aeaf30ac
mehak223454-del <mehak223454@gmail.com>
2026-02-03 01:49:45 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@
 module.exports = {
   SESSION_ID: "",  // add session Id here
   
-  OWNER_NUMBER: "923427582273", // add owner number 
+  OWNER_NUMBER: "923371432785", // add owner number 
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
